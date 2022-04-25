5fa8d02bfc90ebd87989a3f031861675e4cc0b17
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-04-26 02:43:09 +0900

    初期コミット

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as core from '@actions/core'
+import * as io from '@actions/io'
+import * as os from 'os'
+
+const IsMacOS = os.platform() === 'darwin'
+
+async function Run()
+{
+	try {
+		io.rmRF(`${core.getInput('workspace')}/*`)
+	} catch (ex: any) {
+		core.setFailed(ex.message)
+	}
+}
+
+if (!IsMacOS) {
+	core.setFailed('Action requires macOS agent.')
+} else {
+    Run()
+}
